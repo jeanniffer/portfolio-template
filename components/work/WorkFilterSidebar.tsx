23b8836024fd8ba5802d1d0c85db4e7ccd75d09e
Jeanniffer Pimentel <hola@jeanniffer.com>
2026-08-10 17:03:56 -0500

    Add bottom spacing before border line on home gallery

--- components/work/WorkFilterSidebar.tsx
+++ components/work/WorkFilterSidebar.tsx
@@ -15,7 +15,7 @@ export default function WorkFilterSidebar({
   titleB: string;
 }) {
   return (
-    <aside className="sticky top-10 flex h-fit shrink-0 flex-col items-start gap-6 self-start py-6">
+    <aside className="sticky top-10 flex h-fit shrink-0 flex-col items-start gap-6 self-start py-10">
       <motion.div
         initial={{ opacity: 0, y: 12 }}
         animate={{ opacity: 1, y: 0 }}
